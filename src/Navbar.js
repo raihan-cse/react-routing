0710237e4acc436d37b24b980697e2fd32b3f0e6
Raihan Kabir <raihancse46@gmail.com>
2021-03-14 11:54:34 +0600

    fetch api single data

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 function Navbar() {
     return(
         <div>
-            <ul>
+            <ul style={{listStyle: 'none', display: 'flex', justifyContent: 'space-around'}}>
                 <li><Link exact to='/'>Home</Link></li>
                 <li><Link exact to='/About'>About</Link></li>
                 <li><Link exact to='/Contact'>Contact</Link></li>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 
-function Home() { 
+const Home = () => { 
+    const[post, setPost] = useState({});
+    console.log(post);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then(res => res.json())
+        .then(data => setPost(data))
+        .catch(err => console.log(err))
+    },[])
+
         return (
-            <h1>Home Page</h1>
+            <div>
+                {Object.keys(post).length > 0 && (
+                    <>
+                    <h1>Title: {post.title}</h1>
+                    <p>Body: {post.body}</p>
+                    </>
+                )}
+            </div>
         );
-}
+};
  
 export default Home;
